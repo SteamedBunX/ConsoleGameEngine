566ac6fe0f4f948a8a978134bb00ed04c4f411f6
Yi Hao <steamedbunx@yahoo.com>
2019-04-26 09:17:29 -0400

    Adding Object Handler, a class that can be utilized to hold all the UI components. Adding Project Demo. it will be utilized to test and demostrate the use of this class library.

---
+++ ConsoleGameEngine/Components/ObjectHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameEngine.Components
+{
+    public class ObjectHandler
+    {
+        List<Image> images = new List<Image>();
+
+        public void LoadImages(string folderPath)
+        {
+            images.AddRange(Loader.LoadImages(folderPath));
+        }
+
+        public void LoadImage(string path)
+        {
+            images.Add(Loader.LoadImage(path));
+        }
+
+        public void ClearImages()
+        {
+            images.Clear();
+        }
+    }
+}
