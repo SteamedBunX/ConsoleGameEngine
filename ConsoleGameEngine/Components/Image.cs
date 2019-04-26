a81b667c5a832a8a940e7e39df1a7201c95644c5
Yi Hao <steamedbunx@yahoo.com>
2019-04-26 08:21:51 -0400

    Adding basic empty classes

---
+++ ConsoleGameEngine/Components/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameEngine.Components
+{
+    public class Image
+    {
+        public string name;
+        public List<Color> colors = new List<Color>();
+        public List<string> bitmap = new List<string>();
+    }
+}
--- ConsoleGameEngine/Renderer.cs
+++ ConsoleGameEngine/Renderer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleGameEngine
 {
-    public class Class1
+    class Renderer
     {
     }
 }
