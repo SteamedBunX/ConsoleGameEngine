3caf32aee41aa00053b73c146d1ad12d43b7664a
Yi Hao <steamedbunx@yahoo.com>
2019-04-26 08:54:35 -0400

    Image Loading Component inside complete

--- ConsoleGameEngine/Loader.cs
+++ ConsoleGameEngine/Loader.cs
@@ -1,22 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ConsoleGameEngine.Components;
+using Image = ConsoleGameEngine.Components.Image;
 
 namespace ConsoleGameEngine
 {
     public static class Loader
     {
+
+
+
+
+
+
+        // Image Loading Component
         public static List<Image> LoadImages(string folderPath)
         {
-            return null;
+            List<Image> images = new List<Image>();
+            foreach (string f in Directory.GetFiles(folderPath))
+            {
+                if (f.Substring(f.Length - 3).ToLower() == ".ci")
+                {
+                    images.Add(LoadImage(f));
+                }
+            }
+            return images;
         }
 
         public static Image LoadImage(string path)
         {
-            return null;
+            // Get the name of the Image
+            string[] pathParts = path.Split('\\');
+            string name = pathParts.Last();
+            name = name.Split('.')[0];
+
+            StreamReader img = new StreamReader(path);
+
+            //Load the colors
+            List<string> colorsInHax = new List<string>();
+            List<Color> colors = new List<Color>();
+            bool colorSet = false;
+            string nextColor = img.ReadLine();
+            do
+            {
+                if (nextColor == "")
+                {
+                    colorSet = true;
+                }
+                else
+                {
+                    colorsInHax.Add(nextColor);
+                }
+                nextColor = img.ReadLine();
+            } while (!colorSet);
+            colors = FillColor(colorsInHax);
+
+            //Lost the bitmap
+            List<string> bitmap = new List<string>();
+            while (!img.EndOfStream)
+            {
+                bitmap.Add(img.ReadLine());
+            }
+
+            return new Image
+            {
+                Name = name,
+                Colors = colors,
+                BitMap = bitmap
+            };
+        }
+
+        public static List<Color> FillColor(List<string> hexColors)
+        {
+            List<Color> colors = new List<Color>();
+            foreach (string hc in hexColors)
+            {
+                ColorConverter converter = new ColorConverter();
+                colors.Add((Color)converter.ConvertFromString(hc));
+            }
+            return colors;
         }
     }
 }
