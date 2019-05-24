b09afae1590f58c4b1f5c14e6fbfd91598206d76
Yi Hao <yi.hao01@outlook.com>
2019-05-24 13:27:55 -0400

    begin to build the logic for "Digit" class

--- ConsoleGameEngine/Components/Digit.cs
+++ ConsoleGameEngine/Components/Digit.cs
@@ -8,5 +8,41 @@ namespace ConsoleGameEngine.Components
 {
     public class Digit
     {
+        byte number;
+        byte cap;
+
+        #region Constructor
+
+        public Digit(byte number = 0, byte cap = 9)
+        {
+            this.number = number;
+            this.cap = cap;
+        }
+
+        #endregion
+
+        #region Manipulation
+
+        public bool Up()
+        {
+            if (number >= cap)
+            {
+                return true;
+            }
+            number++;
+            return false;
+        }
+
+        public bool Down()
+        {
+            if (number <= 0)
+            {
+                return true;
+            }
+            number--;
+            return false;
+        }
+
+        #endregion
     }
 }
